f7661ca21727430d119daa6bee12eaaefe6faaa3
Olivia Finster <olivia.finster@gmail.com>
2021-06-03 17:25:12 -0500

    GuestList component working

---
+++ src/components/GuestList/GuestList.js
@@ -0,0 +1,30 @@
+function GuestList(props) {
+
+    console.log('trying to log props in GuestList', props);
+
+
+    return (
+        <>
+            <h2>Guest List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Kid's Meal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.guestList.map(guest => (
+                        <tr key={guest.id}>
+                            <td>{guest.name}</td>
+                            <td>{String(guest.kidsMeal)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+
+    )
+}
+
+export default GuestList;
